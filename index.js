6089e4d3449fa13001115ec40aca4753ce7d9c9b
Lauren Auchter <lsauchter@gmail.com>
2019-05-05 20:57:52 -0400

    Added more API data return checks

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 function stationLocation () {
-    fetch('http://api.open-notify.org/iss-now.json')
-    .then(respnose => response.json())
-    .then(responseJson => console.log(responseJson))
-    .catch(error => console.log(error));
+    fetch('http://api.open-notify.org/iss-now.json', {
+    })
+    .then(response => response.json())
+     .then(responseJson => console.log(responseJson))
+     .catch(error => console.log(error));
   }
   
+  function getStationPasses() {
+    $.ajax({
+        url: 'http://api.open-notify.org/iss-pass.json?lat=35&lon=-78&callback=logStationPasses',
+        dataType: "jsonp"
+    })
+}
+
+  function logStationPasses(response) {
+      console.log(response);
+      //need error catch
+  }
+
   function pullMap() {
     var mymap = L.map('mapid').setView([51.505, -0.09], 13);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -16,4 +29,5 @@ function stationLocation () {
   }
   
   stationLocation();
+  getStationPasses();
   pullMap();
